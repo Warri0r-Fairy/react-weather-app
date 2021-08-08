a30de60c63d3e81e43669322aff4aa2e76d054df
Warri0r-Fairy <80551940+Warri0r-Fairy@users.noreply.github.com>
2021-08-08 14:07:08 +1000

    Restart load
    Restarted from scratch due to an issue.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container body">
+      <div className="App">
+        <form>
+          <div className="row row-spacing">
+            <div className="col-8">
+              <input
+                type="text"
+                className="form-control"
+                autofocus="on"
+                placeholder="Search for a city"
+              />
+            </div>
+            <div className="col-2">
+              <input type="submit" className="btn btn-primary" value="Search" />
+            </div>
+            <div className="col-2">
+              <button className="btn btn-success">Current</button>
+            </div>
+          </div>
+        </form>
+        <div className="row row-spacing">
+          <div className="col-6">
+            <div className="current-location">
+              <h1>Melton</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="row row-spacing">
+          <div className="col-4">
+            <p className="date"></p>
+          </div>
+          <div className="col-2">
+            <img
+              src="https://image.flaticon.com/icons/png/512/1163/1163708.png"
+              alt="Weather Icon"
+              className="current-icon"
+            />
+            <span className="temp-now"></span>
+            <span className="units">15°C </span>
+          </div>
+
+          <div className="col-6">
+            <p>
+              <span></span>
+              <br />
+              Humidity: <span>50</span>%
+              <br />
+              Wind: <span>10</span>km/h
+            </p>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+        <footer>
+          <a
+            href="https://github.com/Warri0r-Fairy/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Code open-sourced
+          </a>
+          <span> by Jacqui Lewis</span>
+        </footer>
+      </div>
     </div>
   );
 }
